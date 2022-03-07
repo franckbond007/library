b8cc4e0d4ea6cc0259e4f0ba5b4b0359409397b4
franckbond007 <97653396+franckbond007@users.noreply.github.com>
2022-03-07 00:37:14 -0700

    Create cicd.groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,18 @@
+def newGit(repo)
+{
+  git "${repo}"
+}
+
+def newMvn()
+{
+   sh 'mvn package'
+}
+def newDeploy(ip,appname)
+{
+ deploy adapters: [tomcat9(credentialsId: '64325657-48db-4197-a33f-2c10def1ce99', path: '', url: "${ip}")], contextPath: "${appname}, war: '**/*.war'   
+}    
+
+
+
+
+
