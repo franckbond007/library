4fab7ed187225badafac8c75800e531b254f331f
franckbond007 <97653396+franckbond007@users.noreply.github.com>
2022-03-07 01:05:08 -0700

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -9,7 +9,7 @@ def newMvn()
 }
 def newDeploy(ip,appname)
 {
- deploy adapters: [tomcat9(credentialsId: '64325657-48db-4197-a33f-2c10def1ce99', path: '', url: "${ip}")], contextPath: "${appname}, war: '**/*.war'   
+ deploy adapters: [tomcat9(credentialsId: '64325657-48db-4197-a33f-2c10def1ce99', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'   
 }    
 
 
