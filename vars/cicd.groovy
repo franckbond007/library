0449ff25f436e9bfa4a10d618a4ce2706fc9fc52
franckbond007 <97653396+franckbond007@users.noreply.github.com>
2022-03-08 05:52:16 -0700

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -18,7 +18,7 @@ def newSelenium(path)
  sh 'java -jar "${path}"testing.jar' 
 }
 
-def newDeploy(ip,appname)
+def newDelivery(ip,appname)
 {
   deploy adapters: [tomcat9(credentialsId: '64325657-48db-4197-a33f-2c10def1ce99', path: '', url: "${ip}")], contextPath: "${appname}", war: '**/*.war'
 }
